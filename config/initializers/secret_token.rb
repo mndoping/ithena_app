ca7da68541aa01203eea613db7b2ebd89bfb2609
Muyiwa Jimi-Salami <ojimisa@gmail.com>
2011-12-24 16:18:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IthenaApp::Application.config.secret_token = '3773d9c447055e3127b400af66147b79a7f6a5a6af01a9098ec761bfdfec88079ec7d94bceaabfd45f195eafecd39f9851d360f1adadb1dd84322c7f0b203100'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IthenaApp::Application.config.session_store :cookie_store, key: '_ithena_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IthenaApp::Application.config.session_store :active_record_store
