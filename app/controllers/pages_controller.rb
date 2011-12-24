20a5d96451e1a3a125e47e87f5e84e7b7597ad25
Muyiwa Jimi-Salami <ojimisa@gmail.com>
2011-12-24 16:57:34 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def account
+    @title = "Account"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def faq
+    @title = "Faqs"
   end
 
 end
