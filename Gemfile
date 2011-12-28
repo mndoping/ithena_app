7aa1f695100dc67593bdbbafae1354557b5600bf
Muyiwa Jimi-Salami <ojimisa@gmail.com>
2011-12-27 22:58:44 -0500

    Added uglifier gem

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem "rails-boilerplate"
 group :assets do
   gem 'sass-rails',   '~> 3.1.5'
   gem 'coffee-rails', '~> 3.1.1'
-  #gem 'uglifier', '>= 1.0.3'
+  gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,19 +1,19 @@
 class Company < ActiveRecord::Base
   attr_accessible :company_name, :company_email, :name, :mobile_no, :email, :password, :password_confirmation, :level
-   has_secure_password
-   validates_presence_of :password, :on => :create
+  has_secure_password
+  validates_presence_of :password, :on => :create
 
-   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-   validates :name,         :presence => true,
+  validates :name,          :presence => true,
                             :length   => { :maximum => 50 }
-   validates :company_name, :presence => true,
+  validates :company_name, :presence => true,
                             :length   => { :maximum => 50 }
-   validates :email,        :presence => true,
+  validates :email,         :presence => true,
                             :format   => { :with => email_regex },
                             :uniqueness => { :case_sensitive => false }
-   validates :company_email,:presence => true,
+  validates :company_email, :presence => true,
                             :format   => { :with => email_regex },
                             :uniqueness => { :case_sensitive => false }
-   validates :mobile_no,    :presence => true
+  validates :mobile_no,     :presence => true
 end
